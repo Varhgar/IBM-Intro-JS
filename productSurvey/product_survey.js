9ef4dff58a007fc8452f7ddb93e1f3697750f97f
Varhgar <rmatthies39@gmail.com>
2025-04-24 20:03:06 -0500

    updated product survey

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -9,6 +9,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('userExperience').value;
 
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -17,6 +18,8 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+
+    document.getElementById('displayExperience').innerHTML = experience;
 
     document.getElementById('userInfo').style.display = 'block';
 
