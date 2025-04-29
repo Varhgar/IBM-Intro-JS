ad3cc95c15d8ead9016976a009c4c1586271ce60
Varhgar <rmatthies39@gmail.com>
2025-04-29 17:42:23 -0500

    Added debugging example

---
+++ DebugCode/debug_code.js
@@ -0,0 +1,30 @@
+function performOperation() {
+   //get user input
+   let num1 = parseInt(document.getElementById('input1').value);
+   let num2 = parseInt(document.getElementById('input2').value);
+   //check if inputs are valid numbers
+   if (!isNaN(num1) && !isNaN(num2)) {
+      //perform operation
+      let result = multiply(num1, num2);
+
+      //displat result
+      displayResult(result);
+   } else {
+      displayResult('Please enter valid numbers');
+   }
+}
+
+function multiply(a, b) {
+   // add debugger to pause execution
+   debugger;
+
+   //multiply numbers
+   return a * b;
+}
+
+function displayResult() {
+   const resultElement = document.getElementById('result');
+   resultElement.textContent = `The result is: ${result}`;
+}
+
+            
